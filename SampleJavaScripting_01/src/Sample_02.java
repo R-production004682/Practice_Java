7e6e8ee340f327737450e3a911a6db8de722b2b5
ryousuke <83821881+Ryosuke004682@users.noreply.github.com>
2023-08-05 01:55:55 +0900

    Javaで分岐の処理について書いた。

--- SampleJavaScripting_01/src/Sample_02.java
+++ SampleJavaScripting_01/src/Sample_02.java
@@ -1,6 +1,4 @@
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 
 public class Sample_02
 {
@@ -82,17 +80,17 @@ public class Sample_02
 		
 		
 		//Test＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝
-		BufferedReader br = new BufferedReader(
-                new InputStreamReader( System.in ) );
-
-int x = Integer.parseInt( br.readLine() );
-int y = Integer.parseInt( br.readLine() );
-
-System.out.println("和 " + ( x + y ) );
-System.out.println("差 " + ( x - y ) );
-System.out.println("積 " + ( x * y ) );
-System.out.println("商 " + ( x / y ) );
-System.out.println("余り " + ( x % y ) ); 
+//		BufferedReader br = new BufferedReader(
+//                new InputStreamReader( System.in ) );
+//
+//int x = Integer.parseInt( br.readLine() );
+//int y = Integer.parseInt( br.readLine() );
+//
+//System.out.println("和 " + ( x + y ) );
+//System.out.println("差 " + ( x - y ) );
+//System.out.println("積 " + ( x * y ) );
+//System.out.println("商 " + ( x / y ) );
+//System.out.println("余り " + ( x % y ) ); 
 
 
 /*BufferedReaderは二つ定義する必要はない。
