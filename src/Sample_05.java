9d362fa8b1027b80e7cd084a1cd55e7f1572721c
ryousuke <83821881+Ryosuke004682@users.noreply.github.com>
2023-08-11 23:18:40 +0900

    配列について

---
+++ src/Sample_05.java
@@ -0,0 +1,88 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+//配列について
+public class Sample_05
+{
+	public static void main(String[] ages) throws IOException
+	{
+		//練習問題5-1
+//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//		int[] nums = new int[10] ;
+//		
+//		for(var i = 0; i < nums.length; i++)
+//		{
+//			nums[i] = Integer.parseInt(br.readLine());
+//			System.out.print("");
+//		}
+//		for(var i = 0; i < nums.length; i++)
+//		{
+//			System.out.print(nums[i] * 2);
+//			System.out.print("\n");
+//		}
+		
+		//練習問題5-2
+//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//		int[] nums = new int[10];
+//		
+//		for(var i = 0; i < nums.length; i++)
+//		{
+//			nums[i] = Integer.parseInt(br.readLine());
+//		}
+//		for(var i = 9; i >= 0; i-- )
+//		{
+//			System.out.print(nums[i]);
+//		}
+		
+		//練習問題5-3
+//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//    	int[] nums = new int[10];
+//    	
+//    	for(var i = 0; i < nums.length; i++)
+//    	{
+//    		nums[i] = Integer.parseInt(br.readLine());
+//    		
+//    		System.out.print("\n偶数 : ");
+//    		for(var n : nums) 
+//    		{
+//    			if((n % 2 ) == 0) {System.out.print(n + " "); }
+//    		}
+//    		
+//    		System.out.print("\n奇数 : ");
+//    		for(var n : nums)
+//    		{
+//    			if((n % 2) != 0) {System.out.print(n + " ");}
+//    		}
+//    	}  	
+		
+		//練習問題5-4
+//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//		int[] nums = new int[10];
+//		int cnt , sum;
+//		
+//		for(cnt = 0 , sum = 0 ; cnt < 10 && sum <= 100 ; cnt++)
+//		{
+//			nums[cnt] = Integer.parseInt(br.readLine());
+//			sum += nums[cnt];
+//		}
+//		for(var i = 0; i < cnt; i++) { System.out.print("\n" + nums[i]); }
+		
+		//練習問題5-5
+		BufferedReader br = new BufferedReader(
+        new InputStreamReader( System.in ) );
+		int value = Integer.parseInt( br.readLine() );
+
+		int binary[] = new int[16];
+
+		//配列 binaryに0か1を代入する
+		for( int i = 15 ; i >= 0 ; i--, value /= 2 )
+		    binary[i] = value % 2;
+
+		for( int i = 0 ; i < 16 ; i++ )
+		System.out.print( binary[i] ); 
+		
+		
+	}
+}
+    	
